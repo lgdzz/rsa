8b43fb8d5fa0e4795a8e9b9ac1477db84b272aec
x <xiongwq@foxmail.com>
2019-04-15 14:40:43 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # rsa
 rsa加密解密
+
+### 生成RSA私钥
+```
+# openssl genrsa -out rsa_private.key 2048
+```
+
+### 生成RSA公钥
+```
+openssl rsa -in rsa_private.key -pubout -out rsa_public.key
+```
+
+```
+### 私钥加密 ###
+privateEncrypt
+
+### 公钥解密 ###
+publicDecrypt
+```
+
+```
+### 公钥加密 ###
+publicEncrypt
+
+### 私钥解密 ###
+privateDecrypt
+```
---
+++ src/Rsa.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Created by rsa.
+ * User: lgdz
+ * Date: 2019/4/15
+ */
+
+class Ras
+{
+    private $private_key = '';
+    private $public_key = '';
+
+    public function setPrivateKey(string $private_key)
+    {
+        $this->private_key = $private_key;
+        return $this;
+    }
+
+    public function setPublicKey(string $public_key)
+    {
+        $this->public_key = $public_key;
+        return $this;
+    }
+
+    public function privateEncrypt(string $decrypted)
+    {
+        return $this->_usePrivateKey(function ($private_id) use ($decrypted) {
+            $encrypted = '';
+            foreach (str_split($decrypted, 117) as $chunk) {
+                openssl_private_encrypt($chunk, $temp, $private_id);
+                $encrypted .= $temp;
+            }
+            return $this->urlsafeB64encode($encrypted);
+        });
+    }
+
+    public function privateDecrypt(string $encrypted)
+    {
+        return $this->_usePrivateKey(function ($private_id) use ($encrypted) {
+            $decrypted = '';
+            foreach (str_split($this->urlsafeB64decode($encrypted), 128) as $chunk) {
+                openssl_private_decrypt($chunk, $temp, $private_id);
+                $decrypted .= $temp;
+            }
+            return $decrypted;
+        });
+    }
+
+    public function publicEncrypt(string $decrypted)
+    {
+        return $this->_usePublicKey(function ($public_id) use ($decrypted) {
+            $encrypted = '';
+            foreach (str_split($decrypted, 117) as $chunk) {
+                openssl_public_encrypt($chunk, $temp, $public_id);
+                $encrypted .= $temp;
+            }
+            return $this->urlsafeB64encode($encrypted);
+        });
+    }
+
+    public function publicDecrypt(string $encrypted)
+    {
+        return $this->_usePublicKey(function ($public_id) use ($encrypted) {
+            $decrypted = '';
+            foreach (str_split($this->urlsafeB64decode($encrypted), 128) as $chunk) {
+                openssl_public_decrypt($chunk, $temp, $public_id);
+                $decrypted .= $temp;
+            }
+            return $decrypted;
+        });
+    }
+
+    private function _usePrivateKey($func)
+    {
+        $private_id = openssl_pkey_get_private($this->private_key);
+        if (false === $private_id) {
+            return false;
+        } else {
+            return $func($private_id);
+        }
+    }
+
+    private function _usePublicKey($func)
+    {
+        $public_id = openssl_pkey_get_public($this->public_key);
+        if (false === $public_id) {
+            return false;
+        } else {
+            return $func($public_id);
+        }
+    }
+
+    protected function urlsafeB64encode($string)
+    {
+        $data = base64_encode($string);
+        $data = str_replace(array('+', '/', '='), array('-', '_', ''), $data);
+        return $data;
+    }
+
+
+    protected function urlsafeB64decode($string)
+    {
+        $data = str_replace(array('-', '_'), array('+', '/'), $string);
+        $mod4 = strlen($data) % 4;
+        if ($mod4) {
+            $data .= substr('====', $mod4);
+        }
+        return base64_decode($data);
+    }
+}
